f6d033bfec2304d24633a4f322d5b081b800047d
kawtarbenabdelmoumene-ai <kawtarbenabdelmoumene@gmail.com>
2026-02-07 03:08:20 +0100

    Update API URL to Heroku backend

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'https://khedma4students-backend.up.railway.app/api';
+const API_URL = 'https://khedma4students-backend.herokuapp.com/api';
 
 // ------------------- STUDENTS -------------------
 export const getStudents = () => axios.get(`${API_URL}/students`);
